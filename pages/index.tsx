9181a0784163bf6ea993b997dae84877331411de
TeamAbella060420 <96814164+TeamAbella060420@users.noreply.github.com>
2022-10-29 15:26:22 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,14 +46,14 @@ const About: NextPage = () => {
 };
 
 //!called every time  the page refreshed
-// export const getServerSideProps: GetServerSideProps = async (
-//    context: GetServerSidePropsContext
-// ) => {
-//    const res = await fetch('http://localhost:3000/api/services')
-//    const data = await res.json()
-//    console.log(data)
-//    return { props: { services: data.services } }
-// }
+export const getServerSideProps: GetServerSideProps = async (
+   context: GetServerSidePropsContext
+) => {
+   const res = await fetch('/api/services')
+   const data = await res.json()
+   console.log(data)
+   return { props: { services: data.services } }
+}
 
 //!called only during the build of the project
 //? make sure the server(localhost:3000)[this will receive the request during build] is running on a terminal during the build
