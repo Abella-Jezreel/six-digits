283ba21e39e4851615a6142b095444e996f92791
Abella-Jezreel <teamabella@outlook.com>
2025-02-25 13:48:33 +0000

    Update react version to ^17.0.2 to resolve dependency conflicts

--- docs/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true, // If using the App Router
-  },
-  distDir: "docs/.next", // Ensures Vercel uses the correct build output
-};
-
-module.exports = nextConfig;
